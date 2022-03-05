53391171dd9a25ede470dd2d41babe3c1479dfcf
season <997432833lixuan@gmail.com>
2022-03-05 16:02:11 +0800

    chore: add alias

--- NotCreativePc/vite.config.ts
+++ NotCreativePc/vite.config.ts
@@ -3,5 +3,30 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    // 有两种写法
+    /*
+      // 写法一
+      alias: {
+        "~": "/src"
+      }
+    */
+    // 写法二
+    alias: [
+      {
+        find: '~',
+        replacement: '/src',
+      },
+      {
+        find: '~common',
+        replacement: '/src/common',
+      },
+      {
+        find: '~components',
+        replacement: '/src/components',
+      },
+    ],
+    extensions: ['.ts', '.vue', '.tsx'],
+  },
 })
