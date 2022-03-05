ab65f69be83795224f67a600d95efc5afa59fe08
season <997432833lixuan@gmail.com>
2022-03-05 15:57:06 +0800

    chore: eslint

---
+++ NotCreativePc/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'vue/valid-template-root': 'off',
+  },
+}
