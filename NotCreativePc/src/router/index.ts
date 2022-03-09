6e83ab00dfa19482887b9022a9a7a20bc9d01b6a
season <997432833lixuan@gmail.com>
2022-03-09 22:51:59 +0800

    🐞 fix: not founc path

--- NotCreativePc/src/router/index.ts
+++ NotCreativePc/src/router/index.ts
@@ -24,7 +24,7 @@ const routes: RouteRecordRaw[] = [
       },
       // 404 component
       {
-        path: '/:pathMatch(.*)*',
+        path: ':pathMatch(.*)*',
         name: '404',
         component: () => import('~/layouts/NotFound.vue'),
       },
