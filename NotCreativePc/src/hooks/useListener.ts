a22f2683a7b5d9aafad622e6562ca9b5a09e500e
season <997432833lixuan@gmail.com>
2022-03-10 11:12:24 +0800

    ✨ feat: add watch effect listener

--- NotCreativePc/src/hooks/useListener.ts
+++ NotCreativePc/src/hooks/useListener.ts
@@ -1,5 +1,6 @@
-import { effect } from 'vue'
+import { effect, watchEffect } from 'vue'
 
+// 常用listener 不更新依赖
 export type Listener<K extends keyof WindowEventMap> = (
   this: Window,
   ev: WindowEventMap[K]
@@ -9,6 +10,19 @@ export default <T extends keyof WindowEventMap>(
   listener: Listener<T>
 ) => {
   effect(() => {
+    window.addEventListener(type, listener)
+    return () => {
+      window.removeEventListener(type, listener)
+    }
+  })
+}
+
+// watch effect 更新依赖
+export const useWatchListener = <T extends keyof WindowEventMap>(
+  type: T,
+  listener: Listener<T>
+) => {
+  return watchEffect(() => {
     window.addEventListener(type, listener)
     return () => {
       window.removeEventListener(type, listener)
