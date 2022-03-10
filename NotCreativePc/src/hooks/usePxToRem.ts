0d35676c1fe449100e76395eb840a4fbb94124a3
season <997432833lixuan@gmail.com>
2022-03-10 11:13:05 +0800

    ✨ feat: computed rem

---
+++ NotCreativePc/src/hooks/usePxToRem.ts
@@ -0,0 +1,39 @@
+import { ref, watch, computed, effect } from 'vue'
+import useResize from '~/hooks/useResize'
+
+export default () => {
+  const baseSize = ref(100)
+  const baseWidth = ref(750)
+  const { width, resize } = useResize()
+  const fontSize = computed(
+    () => (width.value / baseWidth.value) * baseSize.value
+  )
+
+  watch(
+    [width],
+    ([preWidth], [newWidth]) => {
+      console.log(newWidth, 'new width')
+      if (preWidth !== newWidth) {
+        baseWidth.value = newWidth
+      }
+    },
+    {
+      flush: 'post',
+    }
+  )
+
+  const updateHtmlFontSize = () => {
+    document.documentElement.style.fontSize = `${fontSize.value}px`
+  }
+
+  effect(() => {
+    resize()
+    updateHtmlFontSize()
+  })
+
+  return {
+    baseSize,
+    baseWidth,
+    updateHtmlFontSize,
+  }
+}
