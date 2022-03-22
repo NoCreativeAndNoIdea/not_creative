9bca3a0133b5bada73b7a7f19692f29d08a5c8cc
season <997432833lixuan@gmail.com>
2022-03-22 10:47:42 +0800

    ✨ feat: add is hidden fn

--- NotCreativePc/src/utils/index.ts
+++ NotCreativePc/src/utils/index.ts
@@ -1,1 +1,15 @@
+import { unref } from 'vue'
+import { MaybeRef } from './types'
+
 export const generateKey = () => Math.random().toString(36).slice(2, 10)
+
+export function isHidden(elementRef: MaybeRef<HTMLElement | undefined>) {
+  const el = unref(elementRef)
+  if (!el) return false
+  const style = window.getComputedStyle(el)
+  const hidden = style.display === 'none'
+
+  const parentHidden = el.offsetParent === null && style.position !== 'fixed'
+
+  return hidden || parentHidden
+}
