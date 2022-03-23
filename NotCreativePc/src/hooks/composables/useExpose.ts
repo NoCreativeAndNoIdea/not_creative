fd899c8e13604f9f0e842a6dbd0ba478b50e6fb8
season <997432833lixuan@gmail.com>
2022-03-23 14:31:27 +0800

    ✨ feat: expose hook

---
+++ NotCreativePc/src/hooks/composables/useExpose.ts
@@ -0,0 +1,8 @@
+import { getCurrentInstance } from 'vue'
+
+export function useExpose<T = Record<string, any>>(apis: T) {
+  const instance = getCurrentInstance()
+  if (instance) {
+    Object.assign(instance?.proxy || {}, apis)
+  }
+}
