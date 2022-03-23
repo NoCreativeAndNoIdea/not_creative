2d0eb42bf4369080c55091c8a29982d3459f1618
season <997432833lixuan@gmail.com>
2022-03-23 17:36:04 +0800

    ✨ feat: event stop fn

--- NotCreativePc/src/utils/dom.ts
+++ NotCreativePc/src/utils/dom.ts
@@ -4,3 +4,18 @@ export function getScrollTop(el: ScrollElement): number {
   const top = 'scrollTop' in el ? el.scrollTop : el.pageYOffset
   return Math.max(top, 0)
 }
+
+export const stopPropagation = (event: Event) => event.stopPropagation()
+
+export function preventDefault(
+  event: Event,
+  isStopPropagation?: boolean
+): void {
+  if (typeof event.cancelable !== 'boolean' || event.cancelable) {
+    event.preventDefault()
+  }
+
+  if (isStopPropagation) {
+    stopPropagation(event)
+  }
+}
